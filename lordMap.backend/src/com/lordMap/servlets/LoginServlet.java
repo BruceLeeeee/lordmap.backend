91c5521a7c55be6f96134b1aeb9efdf6948a0d89
Sichang Li <seeking.scli@gmail.com>
2013-04-25 16:09:43 -0400

    login

--- lordMap.backend/src/com/lordMap/servlets/LoginServlet.java
+++ lordMap.backend/src/com/lordMap/servlets/LoginServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.labs.repackaged.org.json.JSONException;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
 import com.lordMap.datastore.DataStore;
 
 public class LoginServlet extends HttpServlet {
@@ -17,28 +19,36 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		resp.setContentType("application/json");
 		PrintWriter out = resp.getWriter();
 //		out.println("hello!");
 		String userId = req.getParameter("userId");
 		String userPwd = req.getParameter("userPwd");
-		out.println(userId + userPwd);
+//		out.println(userId + userPwd);
 		DataStore ds = new DataStore();
-//		//if id exists
-		if (!ds.checkUserId(userId)) {
-			out.println("Not in datastore");
-		}
-//			if (ds.checkUserPwd(userId, userPwd)) {	
-//				resp.setStatus(1);
-//			}
-//			//pwd is not correct
-//			else {
-//				resp.setStatus(-2);
-//			}
-//		}
-//		else {
-//			resp.setStatus(-1);
-//		}
 		
+		JSONObject result = new JSONObject();
+
+		//if id exists
+		if (ds.checkUserId(userId) && ds.checkUserPwd(userId, userPwd)) {
+//			out.println("Not in datastore");
+			try {
+				result.put("result", "yes");
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else {
+			try {
+				result.put("result", "no");
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		out.print(result);
+		out.flush();
 		
 	}
 
