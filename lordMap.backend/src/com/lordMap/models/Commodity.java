80bd99cb5463ed5a921634c81ff4503c44cd6d2d
BruceLeeeee <cloudsweet1989@gmail.com>
2013-04-27 15:55:52 -0400

    added get inventory function

---
+++ lordMap.backend/src/com/lordMap/models/Commodity.java
@@ -0,0 +1,19 @@
+package com.lordMap.models;
+
+public class Commodity {
+	private int index;
+	
+	public int getIndex() {
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	private String owner;
+}
---
+++ lordMap.backend/src/com/lordMap/models/Inventory.java
@@ -0,0 +1,56 @@
+package com.lordMap.models;
+
+public class Inventory {
+	public static final String[] name = {"Truesilver Champion",
+									"Soulblade of the Breaking Storm",
+									"Crushing Mass of McGowan",
+									"Blazefury, Reborn",
+									"Qon's Flaming Scimitar",
+									"Iron Wall",
+									"Infinity Wall",
+									"Wall of Death",
+									"Wall of Freedom",
+									"Wall of Light"
+									};
+	public static final int[] price = {1000,
+								  2000,
+								  3000,
+								  4000,
+								  5000,
+								  1000,
+								  2000,
+								  3000,
+								  4000,
+								  5000
+								  };	
+	public static final String[] type = {"weapon",
+									"weapon",
+									"weapon",
+									"weapon",
+									"weapon",
+									"wall",
+									"wall",
+									"wall",
+									"wall",
+									"wall"};
+	public static final int[] atkPoint = {500,
+									 1000,
+									 1500,
+									 2000,
+									 2500,
+									 0,
+									 0,
+									 0,
+									 0,
+									 0};
+	public static final int[] defPoint = {0,
+									 0,
+									 0,
+									 0,
+									 0,
+									 500,
+									 1000,
+									 1500,
+									 2000,
+									 2500};
+}
